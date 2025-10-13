c45778a2b64f8d3c08d9a553cbbb0a6b35b84d7b
jacktocash <jacktocash@icloud.com>
2025-10-13 03:24:43 -0400

    basic setup pt 2

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,11 @@
-import { loginToFishbowl } from './fishbowl-integration/api';
-import './App.css';
+import { loginToFishbowl, getInventory } from "./fishbowl-integration/api";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <button onClick={loginToFishbowl}>Login to Fishbowl</button>
+      <button onClick={getInventory}>Get Inventory</button>
     </div>
   );
 }
---
+++ frontend/src/fishbowl-integration/api.ts
@@ -0,0 +1,17 @@
+import axios from "axios";
+
+const BACKEND_BASE_URL = "http://localhost:5001";
+
+export async function loginToFishbowl() {
+  const response = await axios.get(
+    `${BACKEND_BASE_URL}/api/fishbowl/test-token`
+  );
+  console.log(response.data);
+}
+
+export async function getInventory() {
+  const response = await axios.get(
+    `${BACKEND_BASE_URL}/api/fishbowl/inventory`
+  );
+  console.log(response.data);
+}
